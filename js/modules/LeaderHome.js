f6061a1fb7bcbda8a5ec0f36f891eeaaa1a63c9d
Jane Eisenstein <ejane@softweave.com>
2020-11-24 11:47:07 -0500

    WIP on dispatching unloadWorlds action.

--- js/modules/LeaderHome.js
+++ js/modules/LeaderHome.js
@@ -4,8 +4,16 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 
 import RunRowContainer from '../containers/RunRowContainer'
+import {isEmpty} from "lodash";
+import {SimplActions} from "simpl-react/lib/actions";
 
 class LeaderHome extends React.Component {
+
+  componentDidMount() {
+    // unload any loaded worlds
+    const {loadedRun, unloadRunData} = this.props;
+    unloadRunData(loadedRun);
+  }
 
   render() {
     const name = this.props.runuser.first_name + ' ' + this.props.runuser.last_name;
@@ -40,18 +48,33 @@ class LeaderHome extends React.Component {
 LeaderHome.propTypes = {
   runuser: PropTypes.object.isRequired,
   runs: PropTypes.array,
+  loadedRun: PropTypes.object,
+
+  unloadRunData: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
     runuser: state.simpl.current_runuser,
-    runs: state.simpl.run
+    runs: state.simpl.run,
+    loadedRun: state.simpl.loaded_run
   };
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    unloadRunData(loadedRun) {
+      console.log(`mapDispatchToProps.unloadRunData:`);
+      if (!isEmpty(loadedRun)) {
+        dispatch(SimplActions.unloadWorlds(loadedRun)); // 4
+      }
+    }
+  }
+};
+
 const module = connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(LeaderHome);
 
 export default module;
